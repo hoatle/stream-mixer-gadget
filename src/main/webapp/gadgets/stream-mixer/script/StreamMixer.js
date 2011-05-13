d41e1c776ccc14eeda6dc5c64c6f91ae8e3d8639
phuonglm <phuonglm@MindStorms-INF>
2011-05-13 17:11:55 +0700

    update css

--- src/main/webapp/gadgets/stream-mixer/script/StreamMixer.js
+++ src/main/webapp/gadgets/stream-mixer/script/StreamMixer.js
@@ -3,6 +3,20 @@
  */
 (function($) {
   $(document).ready(function() {
-
+		$("#statusForm").show();
+		$("#Activities").show();
+		var activityStream = new ActivityStream();
+		
+		Util.renderActivity(activityStream.getActivities());
+		Util.renderActivity(data);
+		var activityComposerParam = { 
+			"textboxID" : "statusTxt" , "textboxID":"UpdateStatusButton","textboxID": "postToTwitter",
+			onSuccess: function(){
+				
+			},
+			onSuccess: function(){
+			}
+		};
+		var activityComposer = new ActivityComposer(activityComposerParam); 
   });
 })(jQuery);
