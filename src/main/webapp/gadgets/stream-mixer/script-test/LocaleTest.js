8bef51980be80c733c9c0c15c2bc55615675ed64
Hoat Le Van <hoatlevan@gmail.com>
2011-05-15 00:47:09 +0700

    Enhance abit LocaleTest.js

--- src/main/webapp/gadgets/stream-mixer/script-test/LocaleTest.js
+++ src/main/webapp/gadgets/stream-mixer/script-test/LocaleTest.js
@@ -2,20 +2,23 @@
   $(document).ready(function() {
     module("LocaleTest");
 
-    test('check Locale class', function() {
+    test('Locale class', function() {
       ok(Locale, 'Locale class must be defined.');
     });
 
-    test('check Locale.getLang', function() {
+    test('Locale.getLang', function() {
       ok(Locale.getLang, 'Locale.getLang must be defined.');
+      ok(Locale.getLang(), 'Locale.getLang() must not be null');
     });
 
-    test('check Locale.getCountry', function() {
+    test('Locale.getCountry', function() {
       ok(Locale.getCountry, 'Locale.getCountry must be defined.');
+      ok(Locale.getCountry(), 'Locale.getCountry() must not be null.');
     });
 
     test('check Locale.getMsg', function() {
       ok(Locale.getMsg, 'Locale.getMsg must be defined.');
+      ok(Locale.getMsg('just_now'), 'Locale.getMsg(\'just_now\') must not be null.');
     });
   });
 
